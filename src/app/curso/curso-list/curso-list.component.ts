41ad1361467be3b5ba6911b4cd3868f14abb443e
lauths12 <l.carretero@uniandes.edu.co>
2023-11-28 09:20:42 -0500

    entrega2

--- src/app/curso/curso-list/curso-list.component.ts
+++ src/app/curso/curso-list/curso-list.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Curso } from '../Curso';
+import { CursosService } from '../cursos.service';
+
 
 @Component({
-  selector: 'app-curso-list',
-  templateUrl: './curso-list.component.html',
-  styleUrls: ['./curso-list.component.css']
+  selector: 'app-cursos-listar',
+  templateUrl: './cursos-listar.component.html',
+  styleUrls: ['./cursos-listar.component.css']
 })
-export class CursoListComponent implements OnInit {
+export class CursosListarComponent implements OnInit {
+  cursos: Array<Curso> = [];
+  tienecurso: boolean= false;
+  nombres : Array<number> = [];
+  selected: boolean = false;
+  selectedCursos!: Curso;
 
-  
-  constructor() { }
+  constructor(private cursoService: CursosService) { }
+
+  getCursos(): void {
+    this.cursoService.getCursos().subscribe((cursos) => {
+      this.cursos = cursos;
+      this.tienecurso = false;
+    });
+  }
+
+  onSelected(curso: Curso): void {
+    this.selected = true;
+    this.selectedCursos = curso;
+  }
 
   ngOnInit() {
+    this.getCursos();
   }
 
 }
+
